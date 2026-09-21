c211a5f2952d8083b2e411d495eed455d4ddb6ec
bayzz16 <komik28282@gmail.com>
2026-09-21 09:07:07 +0700

    Harden plugin startup and command registration.

--- paper/src/main/java/id/imperial/shop/ImperialShopPlugin.java
+++ paper/src/main/java/id/imperial/shop/ImperialShopPlugin.java
@@ -7,17 +7,35 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class ImperialShopPlugin extends JavaPlugin {
     private Economy economy;
     private ShopService shop;
-    @Override public void onEnable() {
+
+    @Override
+    public void onEnable() {
         saveDefaultConfig();
-        RegisteredServiceProvider<Economy> rsp=getServer().getServicesManager().getRegistration(Economy.class);
-        if(rsp==null){getLogger().severe("Vault economy provider not found.");getServer().getPluginManager().disablePlugin(this);return;}
-        economy=rsp.getProvider(); shop=new ShopService(this,economy);
-        getCommand("shop").setExecutor(new ShopCommand(shop));
-        getCommand("sell").setExecutor(new SellCommand(shop));
-        getCommand("sellall").setExecutor(new SellAllCommand(shop));
-        getCommand("imperialshop").setExecutor(new AdminCommand(this,shop));
-        getServer().getPluginManager().registerEvents(new ShopListener(shop),this);
-        getLogger().info("ImperialShop enabled for Paper/Purpur.");
+
+        RegisteredServiceProvider<Economy> rsp =
+                getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null || rsp.getProvider() == null) {
+            getLogger().severe("Vault economy provider not found. ImperialShop disabled.");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        economy = rsp.getProvider();
+        shop = new ShopService(this, economy);
+
+        if (getCommand("shop") != null) getCommand("shop").setExecutor(new ShopCommand(shop));
+        if (getCommand("sell") != null) getCommand("sell").setExecutor(new SellCommand(shop));
+        if (getCommand("sellall") != null) getCommand("sellall").setExecutor(new SellAllCommand(shop));
+        if (getCommand("imperialshop") != null) {
+            getCommand("imperialshop").setExecutor(new AdminCommand(this, shop));
+        }
+
+        getServer().getPluginManager().registerEvents(new ShopListener(shop), this);
+        getLogger().info("ImperialShop enabled for Paper/Purpur 1.21.x.");
     }
-    public void reloadPlugin(){reloadConfig();shop.reload();}
+
+    public void reloadPlugin() {
+        reloadConfig();
+        shop.reload();
+    }
 }
