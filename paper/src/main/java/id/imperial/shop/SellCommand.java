519b6c5ca370917bc31c488584f9511d1705f29b
bayzz16 <komik28282@gmail.com>
2026-09-21 09:36:22 +0700

    Make /sell open the real SellGUI

--- paper/src/main/java/id/imperial/shop/SellCommand.java
+++ paper/src/main/java/id/imperial/shop/SellCommand.java
@@ -22,7 +22,7 @@ public final class SellCommand implements CommandExecutor {
             player.sendMessage("§cTidak memiliki izin.");
             return true;
         }
-        shop.openSell(player);
+        shop.openSellGui(player);
         return true;
     }
 }
