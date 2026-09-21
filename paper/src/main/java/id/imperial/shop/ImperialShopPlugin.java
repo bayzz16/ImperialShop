6238942b323b1eb3d6f730deb2b8e25ce282ac4a
bayzz16 <komik28282@gmail.com>
2026-09-22 05:17:18 +0700

    Register native Imperial economy through Vault

--- paper/src/main/java/id/imperial/shop/ImperialShopPlugin.java
+++ paper/src/main/java/id/imperial/shop/ImperialShopPlugin.java
@@ -2,12 +2,14 @@ package id.imperial.shop;
 
 import net.milkbowl.vault.economy.Economy;
 import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.Material;
 import org.bukkit.ChatColor;
 
 public final class ImperialShopPlugin extends JavaPlugin {
     private Economy economy;
+    private ImperialEconomy imperialEconomy;
     private ShopService shop;
     private TransactionHistory history;
     private FavoritesStore favorites;
@@ -19,16 +21,29 @@ public final class ImperialShopPlugin extends JavaPlugin {
         getServer().getMessenger().registerIncomingPluginChannel(
                 this, VelocityBridgeListener.CHANNEL, new VelocityBridgeListener(this));
 
-        RegisteredServiceProvider<Economy> registration =
-                getServer().getServicesManager().getRegistration(Economy.class);
+        String providerMode = getConfig().getString("economy.provider", "imperial");
+        if (providerMode.equalsIgnoreCase("imperial")) {
+            imperialEconomy = new ImperialEconomy(this);
+            imperialEconomy.load();
+            getServer().getServicesManager().register(
+                    Economy.class, imperialEconomy, this, ServicePriority.Highest);
+            imperialEconomy.startAutosave();
+            economy = imperialEconomy;
+            getLogger().info("Imperial Economy registered as the Vault economy provider.");
+        } else {
+            RegisteredServiceProvider<Economy> registration =
+                    getServer().getServicesManager().getRegistration(Economy.class);
 
-        if (registration == null || registration.getProvider() == null) {
-            getLogger().severe("Vault economy provider not found. ImperialShop disabled.");
-            getServer().getPluginManager().disablePlugin(this);
-            return;
+            if (registration == null || registration.getProvider() == null) {
+                getLogger().severe("Vault economy provider not found. Set economy.provider: imperial or install an external Vault economy provider.");
+                getServer().getPluginManager().disablePlugin(this);
+                return;
+            }
+
+            economy = registration.getProvider();
+            getLogger().info("Using external Vault economy provider: " + economy.getName());
         }
 
-        economy = registration.getProvider();
         history = new TransactionHistory(this);
         favorites = new FavoritesStore(this);
         shop = new ShopService(this, economy);
@@ -72,6 +87,11 @@ public final class ImperialShopPlugin extends JavaPlugin {
     @Override
     public void onDisable() {
         getServer().getMessenger().unregisterIncomingPluginChannel(this, VelocityBridgeListener.CHANNEL);
+        if (imperialEconomy != null) {
+            getServer().getServicesManager().unregister(Economy.class, imperialEconomy);
+            imperialEconomy.shutdown();
+            imperialEconomy = null;
+        }
         if (history != null) history.shutdown();
         if (favorites != null) favorites.shutdown();
     }
