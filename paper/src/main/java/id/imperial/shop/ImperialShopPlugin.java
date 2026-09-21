5a97d41b5d14bd665ef93a67f18424d68ed0639b
bayzz16 <komik28282@gmail.com>
2026-09-21 09:39:18 +0700

    Register dedicated /sellgui command executor

--- paper/src/main/java/id/imperial/shop/ImperialShopPlugin.java
+++ paper/src/main/java/id/imperial/shop/ImperialShopPlugin.java
@@ -26,6 +26,7 @@ public final class ImperialShopPlugin extends JavaPlugin {
 
         if (getCommand("shop") != null) getCommand("shop").setExecutor(new ShopCommand(shop));
         if (getCommand("sell") != null) getCommand("sell").setExecutor(new SellCommand(shop));
+        if (getCommand("sellgui") != null) getCommand("sellgui").setExecutor(new SellCommand(shop));
         if (getCommand("sellall") != null) getCommand("sellall").setExecutor(new SellAllCommand(shop));
         if (getCommand("sellhand") != null) getCommand("sellhand").setExecutor(new SellHandCommand(shop));
         if (getCommand("imperialshop") != null) {
